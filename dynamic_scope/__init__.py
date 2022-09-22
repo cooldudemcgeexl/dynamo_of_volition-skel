b0c5ddfb8db0557302f0230e99de71f599c40227
Nick Lay <layns@mail.uc.edu>
2022-09-22 16:42:40 -0400

    update repo

--- dynamic_scope/__init__.py
+++ dynamic_scope/__init__.py
@@ -1,13 +1,28 @@
-from typing import Dict, Any, Iterator, Optional
+import inspect
+from ast import Raise
 from collections import abc
 from types import FunctionType
-import inspect
+from typing import Any, Dict, Iterator, Optional
 
 
 class DynamicScope(abc.Mapping):
     def __init__(self):
         self.env: Dict[str, Optional[Any]] = {}
 
+    def __getitem__(self, key: str) -> Optional[Any]:
+        if not self.env.__contains__(key):
+            raise NameError(f"Name '{key}' is not defined.")
+        return self.env[key]
+    
+    def __setitem__(self, key: str, value: Optional[Any]):
+        self.env[key] = value
+
+    def __iter__(self) -> Iterator[str]:
+        return self.env.__iter__()
+    
+    def __len__(self) -> int:
+        return self.env.__len__()
+
 
 def get_dynamic_re() -> DynamicScope:
-    return None
+    return DynamicScope()
