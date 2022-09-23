e6ee90c66372e35c0c37792f4e3c2f19508efeca
Nick Lay <layns@mail.uc.edu>
2022-09-22 23:40:10 -0400

    commit for late_local

--- dynamic_scope/__init__.py
+++ dynamic_scope/__init__.py
@@ -10,19 +10,22 @@ class DynamicScope(abc.Mapping):
         self.env: Dict[str, Optional[Any]] = {}
 
     def __getitem__(self, key: str) -> Optional[Any]:
-        if not self.env.__contains__(key):
+        if key not in self.env.keys():
             raise NameError(f"Name '{key}' is not defined.")
+        if self.env[key] == '__unbound__':
+            raise UnboundLocalError(
+                f"Name '{key}' was referenced before assignment.")
         return self.env[key]
-    
+
     def __setitem__(self, key: str, value: Optional[Any]):
-        if not self.env.__contains__(key):
+        if key not in self.env.keys():
             self.env[key] = value
 
     def __iter__(self) -> Iterator[str]:
         return self.env.__iter__()
-    
+
     def __len__(self) -> int:
-        return self.env.__len__()
+        return len(self.env)
 
 
 def get_dynamic_re() -> DynamicScope:
@@ -31,8 +34,17 @@ def get_dynamic_re() -> DynamicScope:
     for frame_info in stack[1::]:
         frame = frame_info.frame
         free_vars = list(frame.f_code.co_freevars)
-        local_vars =  {key: value for (key, value) in frame.f_locals.items() if not free_vars.__contains__(key)}
+        all_vars = frame.f_code.co_cellvars+frame.f_code.co_varnames
+
+        local_vars = {key: value for (
+            key, value) in frame.f_locals.items() if key not in free_vars}
         for var_name, var_value in local_vars.items():
             dyn_scope[var_name] = var_value
 
-    return 
+        # No local variables in frame, but some variables exist
+        # Therefore, we have unbound variables for this scope
+        if not len(frame.f_locals) and len(all_vars):
+            for var in all_vars:
+                dyn_scope[var] = '__unbound__'
+
+    return dyn_scope
--- skeleton_test.py
+++ skeleton_test.py
@@ -1,7 +1,32 @@
 import unittest
+
 import skeleton_test
+from dynamic_scope import get_dynamic_re
+
+
+def test_late_local():
+    def outer():
+        a = "outer_a"
+
+        def inner():
+            dre = get_dynamic_re()
+            a = "inner_a"
+            return dre
+        return inner()
+    return outer()
+
+
+def test():
+    a = "test_a"
+    b = "test_b"
+    return get_dynamic_re()
+
 
 if __name__ == "__main__":
+
+    dre = test_late_local()
+    #print(f"{dre['a']=}")
+
     suite = unittest.findTestCases(skeleton_test)
     result = unittest.TestResult()
     suite.run(result, debug=True)
